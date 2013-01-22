43a0be1aaa3d20426b25845a316ff700b18e2a82
hmnt <hemant.tank05@gmail.com>
2013-01-22 05:38:36 -0600

    Initial files

---
+++ Docs/Release web.config db sync test.sql
@@ -0,0 +1,4 @@
+SELECT *  FROM Users WHERE id = 1
+SELECT *  FROM MasterClaimStatus WHERE id = 2
+SELECT *  FROM MasterFileTypeDetail WHERE id = 5 or ID = 6
+SELECT *  FROM CustomerOrg WHERE id = 1036
